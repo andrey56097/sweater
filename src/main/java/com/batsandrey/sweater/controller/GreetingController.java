3f82c845a3c173f34540358fee3a1e433877ffc5
andrey56097 <batsandrey@gmail.com>
2022-01-09 22:42:50 +0200

    add sring security dependency add login page add security configs add user for security checking add csrf

--- src/main/java/com/batsandrey/sweater/controller/GreetingController.java
+++ src/main/java/com/batsandrey/sweater/controller/GreetingController.java
@@ -8,7 +8,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import java.util.List;
 import java.util.Map;
 
 @Controller
@@ -16,13 +15,12 @@ public class GreetingController {
     @Autowired
     private MessageRepo messageRepo;
 
-    @GetMapping("/greeting")
-    public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Map<String, Object> model) {
-        model.put("name", name);
+    @GetMapping("/")
+    public String greeting(Map<String, Object> model) {
         return "greeting";
     }
 
-    @GetMapping
+    @GetMapping("/main")
     public String main(Map<String, Object> model) {
         Iterable<Message> messages = messageRepo.findAll();
 
